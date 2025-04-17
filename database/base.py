d32b46e33f949329eef1bc0db656e81bad3bc33d
Jesko <xmaxer29@yandex.ru>
2025-04-17 16:42:43 +0300

    fix db

--- database/base.py
+++ database/base.py
@@ -7,7 +7,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 DATABASE_URL = "mysql+aiomysql://j98603797_fp:U6bsdxhmsrG@mysql.bc9753bc1538.hosting.myjino.ru/j98603797_fp"
 
 # Создаём асинхронный движок
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(DATABASE_URL,
+                             echo=False,
+                             pool_pre_ping=True,  # добавьте это
+                             pool_recycle=3600,  # пересоздавать соединения раз в час (рекомендуется)
+                             connect_args={"init_command": "SET time_zone = '+03:00'"}
+                             )
 
 # Фабрика сессий
 
