d4b982ede2c93748bcc6845ed4405fdc11bba9d6
Jesko <xmaxer29@yandex.ru>
2025-04-27 12:29:05 +0300

    feat: Работа с несколькими ботами

---
+++ database/models/funnel_scheduled_messages.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Integer, String, DateTime, Boolean, ForeignKeyConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from database.base import Base
+import uuid
+
+
+class FunnelScheduledMessage(Base):
+    __tablename__ = 'funnel_scheduled_messages'
+
+    id: Mapped[str] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    bot_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    funnel_base_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    message_id: Mapped[str] = mapped_column(String(100), nullable=False)
+    text: Mapped[str] = mapped_column(String(1000), nullable=False)
+    send_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    sent: Mapped[bool] = mapped_column(Boolean, default=False)
+    active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    user_funnel = relationship("UserFunnelStatus", back_populates="scheduled_messages")
+    bot = relationship("ForpostBotList", back_populates="funnel_scheduled_messages")
+
+    __table_args__ = (
+        # Связь на пользователя
+        ForeignKeyConstraint(
+            ['funnel_base_id'],
+            ['user_funnels_status.id'],
+            ondelete="CASCADE"
+        ),
+        # Связь на бота
+        ForeignKeyConstraint(
+            ['bot_id'],
+            ['forpost_bot_list.id'],
+            ondelete="CASCADE"
+        ),
+    )
