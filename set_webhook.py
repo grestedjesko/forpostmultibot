aced966ebb5bde03acb701a96fc5679a8d76b2ac
Jesko <xmaxer29@yandex.ru>
2025-05-09 17:26:22 +0400

    fix: Логгер в pay webhook:

--- set_webhook.py
+++ set_webhook.py
@@ -8,14 +8,14 @@ from crypto.encrypt_token import decrypt_token
 async def setup_all_webhooks():
     async with async_session_factory() as session:
         bots = (await session.execute(
-            sa.select(ForpostBotList).where(ForpostBotList.id == 2)
+            sa.select(ForpostBotList).where(ForpostBotList.id == 3)
         )).scalars().all()
 
         for bot in bots:
             if bot.token == '0':
                 continue
             decrypted_token = decrypt_token(bot.token)
-            telegram_bot = Bot(token=decrypted_token)
+            telegram_bot = Bot(token="5318319553:AAGb6HNSu-bRwqkM-0lF0RXCsEZqz55yz1c")
             url = f"https://api2.forpost.me/webhook/{bot.id}"
             await telegram_bot.set_webhook(url)
             await telegram_bot.session.close()
