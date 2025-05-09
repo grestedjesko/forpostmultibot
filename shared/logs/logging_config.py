d1eba48ceb3ae92f7b9da8160d1a59585399d054
Jesko <xmaxer29@yandex.ru>
2025-05-09 18:57:35 +0400

    feat: Логгирование в топики

--- shared/logs/logging_config.py
+++ shared/logs/logging_config.py
@@ -12,7 +12,7 @@ def setup_logging(bot: Bot, chat_map: dict[str, int]):
         return logger
 
     # 📄 Файловый хендлер
-    file_handler = logging.FileHandler("user_actions.log", encoding="utf-8")
+    file_handler = logging.FileHandler(f"forpost_{bot.id}.log", encoding="utf-8")
     file_handler.setFormatter(logging.Formatter(
         "%(asctime)s | USER_ID=%(user_id)s | USERNAME=%(username)s | ACTION=%(action)s | %(message)s"
     ))
