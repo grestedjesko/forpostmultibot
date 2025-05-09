ccdd1acbc2be9f6c8a0e3908b02e1486d057ea73
Jesko <xmaxer29@yandex.ru>
2025-05-09 19:04:12 +0400

    fix: Уникальный логгер для каждого бота

--- shared/logs/logging_config.py
+++ shared/logs/logging_config.py
@@ -4,21 +4,18 @@ from aiogram import Bot
 
 
 def setup_logging(bot: Bot, chat_map: dict[str, int]):
-    # Логгер
-    logger = logging.getLogger("user_actions")
+    logger = logging.getLogger(f"user_actions_{bot.id}")
     logger.setLevel(logging.INFO)
 
     if logger.handlers:
         return logger
 
-    # 📄 Файловый хендлер
     file_handler = logging.FileHandler(f"forpost_{bot.id}.log", encoding="utf-8")
     file_handler.setFormatter(logging.Formatter(
         "%(asctime)s | USER_ID=%(user_id)s | USERNAME=%(username)s | ACTION=%(action)s | %(message)s"
     ))
     logger.addHandler(file_handler)
 
-    # 📩 Telegram хендлер
     tg_handler = TelegramLogHandler(bot, chat_map)
     logger.addHandler(tg_handler)
 
