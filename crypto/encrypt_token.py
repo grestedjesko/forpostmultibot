a8baea7e157fb33b1f2bd9c6beed2a5684024e23
Jesko <xmaxer29@yandex.ru>
2025-05-15 14:45:48 +0400

    feat: Логгирование списаний с баланса

--- crypto/encrypt_token.py
+++ crypto/encrypt_token.py
@@ -9,14 +9,12 @@ def encrypt_token(token: str) -> str:
     key = load_key()
     f = Fernet(key)
     encrypted = f.encrypt(token.encode())
-    print(encrypted.decode())
     return encrypted.decode()
 
 def decrypt_token(encrypted_token: str) -> str:
     key = load_key()
     f = Fernet(key)
     decrypted = f.decrypt(encrypted_token.encode())
-    print(decrypted.decode())
     return decrypted.decode()
 
 encrypt_token('5318319553:AAGb6HNSu-bRwqkM-0lF0RXCsEZqz55yz1c')
